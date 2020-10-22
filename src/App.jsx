ec464b47e301075b970fa7fae287b1f682149e44
Igor Emanuel Carvalho da Cruz <igor.cruz4@ufla.br>
2020-10-22 16:48:20 -0300

    aulas 01 a 21.Componente Card #03

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import "./App.css";
+import React from "react";
+
+import Card from "./components/layout/Card";
+
+import Aleatorio from "./components/basicos/Aleatorio";
+import Fragmento from "./components/basicos/Fragmento";
+import ComParametro from "./components/basicos/ComParametro";
+import Primeiro from "./components/basicos/Primeiro";
+
+export default () => (
+    <div className="App">
+        <h1>Fundamentos React</h1>
+
+        <div className="Cards">
+
+            <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
+                <Aleatorio min={1} max={60} />
+            </Card>
+
+            <Card titulo="#03 - Fragmento" color="#E94C6F">
+                <Fragmento />
+            </Card>
+
+            <Card titulo="#02 - Com Parâmetro" color="#E8B71A">
+                <ComParametro
+                    titulo="Situação do Aluno"
+                    aluno="Pedro Silva"
+                    nota={9.3}
+                />
+            </Card>
+
+            <Card titulo="#01 - Primeiro Componente" color="#588C73">
+                <Primeiro></Primeiro>
+            </Card>
+        </div>
+    </div>
+);
